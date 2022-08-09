199f77d4e0f93fdd7640e810107649e2f650278f
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-08-09 23:06:31 +0200

    api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inputPart = document.querySelector(".input-part");
 const inputField = document.querySelector("input");
 
 inputField.addEventListener("keyup", (e) => {
+   // if user presser Enter btn and input value is not empty
    if (e.key == "Enter" && inputField.value != "") {
       requestApi(inputField.value);
    }
@@ -10,5 +11,11 @@ inputField.addEventListener("keyup", (e) => {
 
 function requestApi(city) {
    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f53b0896eeb65abea39156ed39677942`;
-   fetch(api).then((response) => console.log(response.json()));
+   fetch(api)
+      .then((response) => response.json())
+      .then((result) => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+   console.log(info);
 }
