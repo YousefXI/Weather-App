485915ec5a77dc425a6073cdab8dd8aa3460fdfd
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-08-10 23:28:26 +0200

    continuing JavaScript

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector(".wrapper");
 const inputPart = document.querySelector(".input-part");
 const inputField = document.querySelector("input");
+const inputTxt = document.querySelector(".input-txt");
+
+let api;
 
 inputField.addEventListener("keyup", (e) => {
    // if user presser Enter btn and input value is not empty
@@ -10,12 +13,22 @@ inputField.addEventListener("keyup", (e) => {
 });
 
 function requestApi(city) {
-   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f53b0896eeb65abea39156ed39677942`;
+   api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f53b0896eeb65abea39156ed39677942`;
+   fetch(api)
+      .then((response) => response.json())
+      .then((result) => weatherDetails(result));
+}
+
+function fetchData() {
    fetch(api)
       .then((response) => response.json())
       .then((result) => weatherDetails(result));
 }
 
 function weatherDetails(info) {
+   if (info.cod == "404") {
+      inputTxt.classList.add("error");
+      inputTxt.innerText = `${inputField.value} isn't a valid city name`;
+   }
    console.log(info);
 }
